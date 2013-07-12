0981057e5164c54bbf83cb15a97a448e339efbb1
Zach Tellman <ztellman@gmail.com>
2013-07-12 13:19:13 -0700

    add docs

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject rhizome "0.1.9"
-  :description "an easy way to visualize graphs"
+  :description "a simple way to visualize graphs"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies []
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]]}})
+  :plugins [[codox "0.6.4"]]
+  :codox {:writer codox-md.writer/write-docs}
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]
+                                  [codox-md "0.2.0" :exclusions [org.clojure/clojure]]]}})
