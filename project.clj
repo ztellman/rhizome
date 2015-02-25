7e000b130f8889bc879d8fc817b9b833933686c0
Zach Tellman <ztellman@gmail.com>
2015-02-24 16:35:29 -0800

    mark 0.2.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject rhizome "0.2.4-SNAPSHOT"
+(defproject rhizome "0.2.4"
   :description "a simple way to visualize graphs"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
