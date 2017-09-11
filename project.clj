b3695e862fe07e76401307449679b78744471224
ztellman <ztellman@gmail.com>
2017-09-11 14:19:38 -0700

    mark 0.2.9

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject rhizome "0.2.8"
+(defproject rhizome "0.2.9"
   :description "a simple way to visualize graphs"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
